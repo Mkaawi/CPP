f23f3237faf9d0e3fad9e87ba1700a7a80ada60c
Mkaawi <ENmalik37@gmail.com>
2025-09-30 19:54:36 -0400

    hh

--- CPP07/ex00/main.cpp
+++ CPP07/ex00/main.cpp
@@ -6,6 +6,20 @@ int main ()
     int a = 4;
     int b = 5;
 
+    std::string s1 = "hamid";
+    std::string s2 = "doban";
+
+    std::cout << "before : a : " << a << "  / b : " << b << std::endl;
     ::swap(a, b);
-    std::cout << "a : " << a << " b : " << b << std::endl;
+    std::cout << "after : a : " << a << "  / b : " << b << std::endl;
+
+    std::cout << "min(a, b) : " << ::min(a, b) << std::endl;
+    std::cout << "max(a, b) : " << ::max(a, b) << std::endl;
+    
+    std::cout << "before : s1 : " << s1 << "  / s2 : " << s2 << std::endl;
+    ::swap(s1, s2);
+    std::cout << "after : s1 : " << s1 << "  / s2 : " << s2 << std::endl;
+
+    std::cout << "min(s1, s2) : " << ::min(s1, s2) << std::endl;
+    std::cout << "max(s1, s2) : " << ::max(s1, s2) << std::endl;
 }
